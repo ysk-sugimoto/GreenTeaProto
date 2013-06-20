e7fba16ecf64c0ceb4e608a5899453e6684196e8
yusuke <yuusuke.sugimoto@nts.ricoh.co.jp>
2013-06-20 19:25:04 +0900

    create new app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GreenTeaProto::Application.config.secret_token = 'c068e95a7ad937efa049d853de70474d60fab028237d5b3bf40f4ca880cc8ce9d25d3d98e35116b8b57948d76670cef55d474a67d54592dbf19e4f1ce683a108'
