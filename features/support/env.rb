38347ce1351985602a159f61943f4b1056dca20b
yusuke <yuusuke.sugimoto@nts.ricoh.co.jp>
2013-06-22 12:46:06 +0900

    add   require 'cucumber/rails'

--- features/support/env.rb
+++ features/support/env.rb
@@ -9,7 +9,7 @@ require 'spork'
  
 Spork.prefork do
   require 'cucumber/rails'
-
+  require 'factory_girl_rails'
 
   # Capybara defaults to CSS3 selectors rather than XPath.
   # If you'd prefer to use XPath, just uncomment this line and adjust any
